4df0c6356e2b925f19bc9f1d5ce0ae5879f03ce3
Toko122 <toko.migine@gmail.com>
2025-09-27 18:58:15 +0400

    n

--- ecommerace-2/src/components/SelectedItem.jsx
+++ ecommerace-2/src/components/SelectedItem.jsx
@@ -56,7 +56,7 @@ const SelectedItem = () => {
         <div className="flex flex-col md:flex-row gap-16 mt-4">
           <div className="flex gap-3">
             <div className="border border-gray-500/30 max-w-100 rounded overflow-hidden">
-              <img src={`https://next-buy-eight.vercel.app/${product.image}`} alt="Selected product" className="w-full h-full object-cover" />
+              <img src={`https://next-buy-eight.vercel.app/uploads/${product.image}`} alt="Selected product" className="w-full h-full object-cover" />
             </div>
           </div>
 
