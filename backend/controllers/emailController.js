1cbffa8bdc63808eff339c36d4cb85c99c587f0a
Toko122 <toko.migine@gmail.com>
2025-09-27 18:51:15 +0400

    n

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -17,7 +17,7 @@ exports.sendEmail = async (req, res) => {
         user.resetTokenExpire = Date.now() + 1000 * 60 * 15
         await user.save()
 
-        const resetLink = `https://next-liw1tep8z-toko122s-projects.vercel.app/reset-password/${resetToken}`
+        const resetLink = `https://next-buy-eight.vercel.app/reset-password/${resetToken}`
 
         const transporter = await nodeMailer.createTransport({
             service: 'gmail',
