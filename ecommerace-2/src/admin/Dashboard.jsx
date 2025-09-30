4e1251bf42b4b1d88fcd5086d0cf7940295110eb
Toko122 <toko.migine@gmail.com>
2025-09-30 17:45:47 +0400

    Update Dashboard.jsx

--- ecommerace-2/src/admin/Dashboard.jsx
+++ ecommerace-2/src/admin/Dashboard.jsx
@@ -83,32 +83,47 @@ const Dashboard = () => {
 
                 
                 <div className="grid gap-4 md:hidden">
-                    {products.map((product, index) => (
-                        <tr key={product._id} className="hover:bg-gray-50 transition duration-150 flex flex-col">
-                        <td className="py-3 px-4 border-b text-center">{index + 1}</td>
-                        <td className="py-3 px-4 border-b">
-                            <div className="flex justify-center">
-                                <img
-                                    src={`https://nextbuy-xpvm.onrender.com${product.image}`}
-                                    className="w-16 h-16 object-cover rounded-md"
-                                />
-                            </div>
-                        </td>
-                        <td className="py-3 px-4 text-center border-b max-w-[200px] truncate">{product.title}</td>
-                        <td className="py-3 px-4 border-b text-center font-semibold">${product.price.toFixed(2)}</td>
-                        <td className="py-3 px-4 border-b text-center">
-                            <button className="bg-blue-600 text-white cursor-pointer px-3 py-1 rounded-md hover:bg-blue-700 transition">
-                                Edit
-                            </button>
-                            <button 
-                                onClick={() => handleDelete(product._id)} 
-                                className="bg-red-600 cursor-pointer text-white px-3 py-1 rounded-md ml-2 hover:bg-red-700 transition"
-                            >
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-                    ))}
+                <div className="overflow-x-auto shadow-md rounded-lg hidden md:block">
+                    <table className="min-w-full border-collapse">
+                        <thead className="bg-gray-100 text-gray-700 text-left flex flex-col">
+                            <tr>
+                                <th className="py-3 px-4 border-b text-center">#</th>
+                                <th className="py-3 px-4 border-b text-center">Image</th>
+                                <th className="py-3 px-4 border-b text-center">Title</th>
+                                <th className="py-3 px-4 border-b text-center">Price</th>
+                                <th className="py-3 px-4 border-b text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products.map((product, index) => (
+                                <tr key={product._id} className="hover:bg-gray-50 transition duration-150 flex flex-col">
+                                    <td className="py-3 px-4 border-b text-center">{index + 1}</td>
+                                    <td className="py-3 px-4 border-b">
+                                        <div className="flex justify-center">
+                                            <img
+                                                src={`https://nextbuy-xpvm.onrender.com${product.image}`}
+                                                className="w-16 h-16 object-cover rounded-md"
+                                            />
+                                        </div>
+                                    </td>
+                                    <td className="py-3 px-4 text-center border-b max-w-[200px] truncate">{product.title}</td>
+                                    <td className="py-3 px-4 border-b text-center font-semibold">${product.price.toFixed(2)}</td>
+                                    <td className="py-3 px-4 border-b text-center">
+                                        <button className="bg-blue-600 text-white cursor-pointer px-3 py-1 rounded-md hover:bg-blue-700 transition">
+                                            Edit
+                                        </button>
+                                        <button 
+                                            onClick={() => handleDelete(product._id)} 
+                                            className="bg-red-600 cursor-pointer text-white px-3 py-1 rounded-md ml-2 hover:bg-red-700 transition"
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
                 </div>
             </div>
         </div>
