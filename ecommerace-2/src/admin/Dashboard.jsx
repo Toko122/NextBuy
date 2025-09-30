56b3211ee3d0bd3c556a3411b0c09b33a9ad5762
Toko122 <toko.migine@gmail.com>
2025-09-30 17:51:06 +0400

    Update Dashboard.jsx

--- ecommerace-2/src/admin/Dashboard.jsx
+++ ecommerace-2/src/admin/Dashboard.jsx
@@ -84,8 +84,8 @@ const Dashboard = () => {
                 
                 <div className="overflow-x-auto shadow-md rounded-lg sm:hidden flex">
                     <table className="min-w-full border-collapse">
-                        <thead className="bg-gray-100 text-gray-700 text-left flex flex-col">
-                            <tr>
+                        <thead className="bg-gray-100 text-gray-700 text-left">
+                            <tr className='flex flex-col'>
                                 <th className="py-3 px-4 border-b text-center">#</th>
                                 <th className="py-3 px-4 border-b text-center">Image</th>
                                 <th className="py-3 px-4 border-b text-center">Title</th>
@@ -95,7 +95,7 @@ const Dashboard = () => {
                         </thead>
                         <tbody>
                             {products.map((product, index) => (
-                                <tr key={product._id} className="hover:bg-gray-50 transition duration-150 flex flex-col">
+                                <tr key={product._id} className="hover:bg-gray-50 transition duration-150 flex flex-col items-start">
                                     <td className="py-3 px-4 border-b text-center">{index + 1}</td>
                                     <td className="py-3 px-4 border-b">
                                         <div className="flex justify-center">
