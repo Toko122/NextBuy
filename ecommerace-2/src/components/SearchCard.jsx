58fa5dea8f73ffbe698f7ac95e42e7aca1cb2c1e
Toko122 <toko.migine@gmail.com>
2025-10-02 17:38:22 +0400

    Update SearchCard.jsx

--- ecommerace-2/src/components/SearchCard.jsx
+++ ecommerace-2/src/components/SearchCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const SearchCard = ({product}) => {
+const SearchCard = ({ product }) => {
   return (
-    <div className='flex gap-4 items-center cursor-pointer'>
-        <img src={product.image} />
-        <h1 className='text-semibold'>{product.title}</h1>
+    <div className='flex gap-3 items-center'>
+      <img src={product.image} alt={product.title} className="w-12 h-12 object-cover rounded" />
+      <h1 className='font-medium text-sm'>{product.title}</h1>
     </div>
   )
 }
